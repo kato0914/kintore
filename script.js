0af5454e4d39e963679ed9ca552ae128629526c2
kato0914 <88483254+kato0914@users.noreply.github.com>
2024-06-14 10:15:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,12 +26,10 @@ function submitForm() {
   });
 }
 
-function () {
-    var today = new Date();
-    today.setDate(today.getDate());
-    var yyyy = today.getFullYear();
-    var mm = ("0"+(today.getMonth()+1)).slice(-2);
-    var dd = ("0"+today.getDate()).slice(-2);
-    document.getElementById("today").value=yyyy+'-'+mm+'-'+dd;
+class InputDateDefault extends HTMLInputElement {
+    connectedCallback() {
+        let now = new Date();
+        this.value = now.toLocaleDateString('sv-SE');
+    }
 }
-
+customElements.define('default-date', InputDateDefault, { extends: 'input', });
